8a0ff5222e2b071ebd086aa5c83419b1ec119747
bmzgm <83974719+bmzgm@users.noreply.github.com>
2022-02-24 17:30:47 +0530

    Add files via upload

---
+++ initialize.js
@@ -0,0 +1,6 @@
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l1.js'></script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l2.js'></script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l3.js'></script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l4.js'></script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l5.js'></script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l6.js'></script>

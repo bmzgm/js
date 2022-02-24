ab8eafe4fbc8a8ec0ffd8ef5bf2efaacabe174dd
bmzgm <83974719+bmzgm@users.noreply.github.com>
2022-02-24 17:42:01 +0530

    Add files via upload

--- initialize.js
+++ initialize.js
@@ -1,6 +1,17 @@
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l1.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l2.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l3.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l4.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l5.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/bmzgm/js@main/l6.js'></script>
+function include(file) {
+  
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+  
+  document.getElementsByTagName('head').item(0).appendChild(script);
+  
+}
+  
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l1.js');
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l2.js');
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l3.js');
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l4.js');
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l5.js');
+include('https://cdn.jsdelivr.net/gh/bmzgm/js@main/l6.js');
